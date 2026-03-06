a7e93a7a9c8ae6edeea61a8f810596273ebb5693
Lucifer <123030767+Abdul-Azeez04@users.noreply.github.com>
2026-03-06 05:52:25 +0100

    feat: add supabase/functions/email-automation/index.ts

---
+++ supabase/functions/email-automation/index.ts
@@ -0,0 +1,5 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { createClient } from "jsr:@supabase/supabase-js@2";
+Deno.serve(async (req: Request) => {
+  return new Response(JSON.stringify({message: "hello"}));
+});
